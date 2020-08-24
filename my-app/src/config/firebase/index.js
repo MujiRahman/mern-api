12b539296e0434d3387f1f4c23d2adc31c1ea0e5
Muji Rahman <mujirahman888@gmail.com>
2020-08-25 03:58:15 +0700

    penataan file

---
+++ my-app/src/config/firebase/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBI6vYNLImY64pKlTUnyQQuSfBWw6_dnhY",
+    authDomain: "simple-note-list.firebaseapp.com",
+    databaseURL: "https://simple-note-list.firebaseio.com",
+    projectId: "simple-note-list",
+    storageBucket: "simple-note-list.appspot.com",
+    messagingSenderId: "1044466240765",
+    appId: "1:1044466240765:web:9f18ff873bb1f1ae17c294",
+    measurementId: "G-2GLPSNXL9P"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
