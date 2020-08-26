181828e4852ec7ef42bc9020844ee9d83a5a62fb
Muji Rahman <mujirahman888@gmail.com>
2020-08-26 21:07:25 +0700

    modif router

--- my-app/src/containers/pages/App/index.js
+++ my-app/src/containers/pages/App/index.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from '../../../assest/img/logo/logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import Dashboard from '../Dashboard';
+import Register from '../Register';
+import Login from '../Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Switch> */}
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        {/* </Switch> */}
+      </div>
+    </Router>
   );
 }
+// function Dashboard() {
+//   return <h2>Dashboard</h2>;
+// }
+
+// function Register() {
+//   return <h2>Register</h2>;
+// }
+
+// function Login() {
+//   return <h2>Login</h2>;
+// }
 
 export default App;
