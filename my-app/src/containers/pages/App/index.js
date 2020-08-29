19c58aeb76a26b1f97ccc476aaab438ce90bc9ac
Muji Rahman <mujirahman888@gmail.com>
2020-08-29 08:37:45 +0700

    menambahkan redux thunk

--- my-app/src/containers/pages/App/index.js
+++ my-app/src/containers/pages/App/index.js
@@ -1,36 +1,26 @@
 import React from 'react';
 // import logo from '../../../assest/img/logo/logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import Dashboard from '../Dashboard';
 import Register from '../Register';
 import Login from '../Login';
+import { Provider } from 'react-redux';
+import { store } from '../../../config/redux'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* <Switch> */}
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        {/* </Switch> */}
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          {/* <Switch> */}
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          {/* </Switch> */}
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
